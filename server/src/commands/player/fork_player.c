2ca6201f8893da4b0dd0a053d01b0ee75cbbe46a
KC <carttigueane.k@hotmail.com>
2023-06-18 18:24:06 +0200

    🔨 fix(server): Fork player command

--- server/src/commands/player/fork_player.c
+++ server/src/commands/player/fork_player.c
@@ -22,35 +22,6 @@ void fork_player(__attribute_maybe_unused__ list_args_t* args)
 
     player_t* player = args->client->player;
 
-    client_t* new_client = (client_t*)calloc(1, sizeof(client_t));
-
-    if (!new_client)
-        return;
-
-    memset(new_client->write_buf, 0, MAX_W_BUFFER_LENGTH);
-    memset(new_client->read_buf, 0, MAX_BUFFER);
-    new_client->player = NULL;
-
-    new_client->player = (player_t*)calloc(1, sizeof(player_t));
-
-    if (!new_client->player)
-        return;
-
-    new_client->player->is_graphical = false;
-    new_client->player->id = args->server_data->game.next_player_id++;
-    new_client->player->pos.x = player->pos.x;
-    new_client->player->pos.y = player->pos.y;
-    new_client->player->orientation = NORTH;
-    new_client->player->level = 1;
-    memset(new_client->player->inventory, 0,
-           sizeof(new_client->player->inventory));
-    new_client->player->life_units = 10;
-    // new_client->player->last_eat_time; // TODO : set this to current time I guess
-    new_client->player->state = EGG;
-
-    init_command_queue(new_client);
-    add_client(&args->server_data->game, new_client);
-
     char response[256] = {0};
 
     sprintf(response, PFK_FORMAT, player->id);
@@ -62,17 +33,21 @@ void fork_player(__attribute_maybe_unused__ list_args_t* args)
     if (!team)
         return;
 
-    egg_t* egg = create_and_add_egg(team, new_client->player->pos);
+    egg_t* egg = create_and_add_egg(team, player->pos);
 
     if (!egg)
         return;
 
     char response2[256] = {0};
 
-    sprintf(response2, ENW_FORMAT, egg->id, new_client->player->id,
-            new_client->player->pos.x, new_client->player->pos.y);
+    sprintf(response2, ENW_FORMAT, egg->id, player->id, player->pos.x,
+            player->pos.y);
 
     append_to_gui_write_buffer(args->server_data, response2);
 
-    printf("New egg created with id %ld\n", new_client->player->id);
+    append_to_string(args->client->write_buf, OK_FORMAT);
+
+    team->max_players += 1;
+
+    printf("New egg created with id %ld\n", player->id);
 }
