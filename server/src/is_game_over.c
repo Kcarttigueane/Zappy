0cb6e6555d9b8b14a21f65c95f3da3d9bd6d8e01
KC <carttigueane.k@hotmail.com>
2023-06-20 14:26:01 +0200

    🔨 fix(server): is game over fucntion

--- server/src/is_game_over.c
+++ server/src/is_game_over.c
@@ -12,13 +12,14 @@ bool is_game_over(server_data_t* s)
     game_t* game = &s->game;
     char response[MAX_BUFFER] = {0};
 
-    for (size_t i = 0; game->team_names != NULL; i++) {
+    for (size_t i = 0; i < s->game.team_count; i++) {
         int players_at_max_level = 0;
-        client_t* client;
+        client_t *client, *temp;
 
-        LIST_FOREACH(client, &game->client_list, entries)
+        LIST_FOREACH_SAFE(client, &s->game.client_list, entries, temp)
         {
-            if (!strcmp(client->player->team_name, game->team_names[i])) {
+            if (client->player->team_name != NULL &&
+                !strcmp(client->player->team_name, game->team_names[i])) {
                 if (client->player->level >= MAX_LEVEL) {
                     ++players_at_max_level;
                 }
